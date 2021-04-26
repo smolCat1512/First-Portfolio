885f158c4deeb3352698ed520a41a6d9cd938d77
ShaunH1512 <24032620@edgehill.ac.uk>
2021-04-26 15:26:48 +0100

    Home page near done now, will move on to about page next

--- javascript/fade.js
+++ javascript/fade.js
@@ -63,3 +63,37 @@ function scrollAppear4() {
 }
 
 window.addEventListener('scroll', scrollAppear4);
+
+
+/*  scrollAppear5 function, when scroll down page any div or container
+ *   containing the desired elements
+ */
+
+function scrollAppear5() {
+    var fadeInText2 = document.querySelector('.fade-in-contact');
+    var textPosition2 = fadeInText2.getBoundingClientRect().top;
+    var screenPosition2 = window.innerHeight / 1.3;
+
+    if (textPosition2 < screenPosition2) {
+        fadeInText2.classList.add('text-appear-contact');
+    }
+}
+
+window.addEventListener('scroll', scrollAppear5);
+
+
+/*  scrollAppear6 function, when scroll down page any div or container
+ *   containing the desired elements
+ */
+
+function scrollAppear6() {
+    var fadeInText2 = document.querySelector('.fade-in-contact-image');
+    var textPosition2 = fadeInText2.getBoundingClientRect().top;
+    var screenPosition2 = window.innerHeight / 1.3;
+
+    if (textPosition2 < screenPosition2) {
+        fadeInText2.classList.add('image-appear-contact');
+    }
+}
+
+window.addEventListener('scroll', scrollAppear6);
