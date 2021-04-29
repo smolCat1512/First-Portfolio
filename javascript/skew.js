0ecef289eeb79bb05d06984ce598e55982875c13
ShaunH1512 <24032620@edgehill.ac.uk>
2021-04-29 13:43:49 +0100

    Added new content to banner, implemented setting for mobile screens to moved text closer to top of screen and shrink text

--- javascript/skew.js
+++ javascript/skew.js
@@ -1,3 +1,5 @@
+// Setup content constant, this takes in class constant which in
+// html is a div wrapped around the whole body
 const content = document.querySelector(".content");
 let currentPixel = window.pageYOffset
 
@@ -5,10 +7,9 @@ let currentPixel = window.pageYOffset
 const looper = function () {
   const newPixel = window.pageYOffset;
   const diff = newPixel - currentPixel
-  const speed = diff * 0.35;
+  const speed = diff * 0.15;
   
   content.style.transform = "skewY(" + speed + "deg)"
-  
   
   currentPixel = newPixel;
   
