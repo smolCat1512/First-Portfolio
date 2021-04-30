ec8724042f29ab97cf4b6af63c2bb77e0aa3ec90
ShaunH1512 <24032620@edgehill.ac.uk>
2021-04-30 14:15:52 +0100

    Removed jquery usage and requirement for the return to top button

--- javascript/top.js
+++ javascript/top.js
@@ -4,14 +4,11 @@
 const topButton = document.getElementById('topButton');
 
 // Functionality to actually work on click
-topButton.addEventListener('click', function() {
-    
-    // window.scrollTo({
-    //     top: 0,
-    //     left: 0,
-    //     behavior: "smooth"
+topButton.addEventListener('click', function () {
 
-    // Using jQuery for animation, this is the most compatible
-    // way for the most browser compatability coverage
-    $("html, body").animate({ scrollTop: 0}, "slow");
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
 });
